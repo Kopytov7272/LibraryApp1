cb629c983eb433a03cdb1e8330ca3ba495f76a8c
Ilya <kopytov.ilya.1999@gmail.com>
2026-01-28 23:14:40 +0300

    добавили book

--- LibraryApp,/Program.cs
+++ LibraryApp,/Program.cs
@@ -1,15 +1,16 @@
-﻿using System;
-using System.Collections.Generic;
-using System.Linq;
-using System.Text;
-using System.Threading.Tasks;
+﻿using LibraryApp.Models;
 
-namespace LibraryApp_
+namespace LibraryApp
 {
-    internal class Program
+    class Program
     {
         static void Main(string[] args)
         {
+            Book myBook = new Book();
+            myBook.Title = "1984";
+            myBook.Author = "Джордж Оруэлл";
+            myBook.Year = 1949;
+            myBook.DisplayInfo();
         }
     }
 }
